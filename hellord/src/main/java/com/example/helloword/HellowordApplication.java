e07ac8e3faf04dd77c5bbc7c4d6564c8250864e1
zhe1 <39737135+zhe1@users.noreply.github.com>
2019-09-27 13:59:17 +0800

    Update HellowordApplication.java

--- hellord/src/main/java/com/example/helloword/HellowordApplication.java
+++ hellord/src/main/java/com/example/helloword/HellowordApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HellowordApplication {
+    
     public static void main(String[] args) {
         SpringApplication.run(HellowordApplication.class, args);
     }
+    
 
 }
